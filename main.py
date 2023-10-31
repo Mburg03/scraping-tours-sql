dff38c303ed8d261b28134a70c3666d68c4ba0e9
Mario Umaña <mburgosgit003@gmail.com>
2023-10-30 23:32:32 -0600

    Add: extract.yaml and extract info from url

--- main.py
+++ main.py
@@ -8,7 +8,19 @@ def scrape(url):
     """Scrape the page source from the URL"""
     response = requests.get(url)
     source = response.text
+    
     return source
 
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    
+    return value
+
+
 if __name__ == "__main__":
-    print(scrape(URL))
+    scraped = scrape(URL)
+    extracted = extract(scraped)
+    print(extracted)
+    
