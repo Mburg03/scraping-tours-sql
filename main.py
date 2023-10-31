86f5e54faef7d912c72bd1e876455516eb9dafed
Mario Umaña <mburgosgit003@gmail.com>
2023-10-31 11:54:09 -0600

    sending data to SQL database instead of .csv file

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import requests
 import selectorlib
 import time
+import sqlite3
 from functions import send_email
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 # "INSERT INTO events VALUES ('Tigers', 'Tigers City', '2023.10.10')"
 # "SELECT * FROM events WHERE date='2023.10.15'"
+connection = sqlite3.connect("./data.db")
+
 
 def scrape(url):
     """Scrape the page source from the URL"""
@@ -16,31 +19,40 @@ def scrape(url):
 
 
 def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    extractor = selectorlib.Extractor.from_yaml_file("./extract.yaml")
     value = extractor.extract(source)["tours"]
     
     return value
 
 
 def store(extracted):
-    with open('data.txt', 'a') as file:
-        file.write(extracted + "\n")        
-
+    row = extracted.split(',')
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
 
 def read_extracted(extracted):
-    with open('data.txt', 'r') as file:
-        return file.read()
+    row = extracted.split(',')
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", 
+                   (band, city, date))
+    rows = cursor.fetchall()
+    return rows
 
-
+    
 if __name__ == "__main__":
     while True:
         scraped = scrape(URL)
         extracted = extract(scraped)
-        content = read_extracted(extracted)
         print(extracted)
         
         if extracted != "No upcoming tours":
-            if extracted not in content:
+            row = read_extracted(extracted)
+            if not row:
                 store(extracted)
                 # send_email(message="Hey, new event was found!")
                 
